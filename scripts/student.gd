6efe0733d3f0b0b6036f88cb160421c304cc2891
Jack206Man <95452460+Jack206Man@users.noreply.github.com>
2026-06-06 14:20:15 -0400

    Changed movement code, possibly fixed bug

--- scripts/student.gd
+++ scripts/student.gd
@@ -7,9 +7,13 @@ const ACCEL: float = 8
 const DEACCEL: float = 16
 
 var cur_speed: float = 4
+var cur_speed_2: float = 4
+
+var collisionNode
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	collisionNode = get_node("Collision")
 
 func _input(event) -> void:
 	if event is InputEventMouseMotion:
@@ -22,6 +26,7 @@ func _physics_process(delta) -> void:
 	
 	if move_direction:
 		cur_speed = move_toward(cur_speed, MAX_SPEED, ACCEL * delta)
+		cur_speed_2 = cur_speed
 		velocity.x = cur_speed * move_direction.x
 		velocity.z = cur_speed * move_direction.z
 		# Check if running
@@ -30,10 +35,23 @@ func _physics_process(delta) -> void:
 			velocity.x *= 2
 			velocity.z *= 2
 	else:
-		cur_speed = 0
+		cur_speed_2 = move_toward(cur_speed_2, 0, DEACCEL * delta)
 		# I think this isn't accounting fully for where the player is facing - Jack 
-		velocity.x = move_toward(velocity.x, cur_speed, DEACCEL * delta)
-		velocity.z = move_toward(velocity.z, cur_speed, DEACCEL * delta)
-
+		velocity.x *= (cur_speed_2 / MAX_SPEED)
+		velocity.z *= (cur_speed_2 / MAX_SPEED)
+		
+		cur_speed = 0
+		
+		# I think this isn't accounting fully for where the player is facing - Jack 
+		#cur_speed = 0
+		#velocity.x = move_toward(velocity.x, cur_speed, DEACCEL * delta)
+		#velocity.z = move_toward(velocity.z, cur_speed, DEACCEL * delta)
+	
+	print("cur_speed: " + str(cur_speed))
+	print("cur_speed_2: " + str(cur_speed_2))
+	print("VX: " + str(velocity.x))
+	print("VZ: " + str(velocity.z))
+	print("VXMT: " + str(move_toward(velocity.x, cur_speed, DEACCEL * delta)))
+	print("VZMT: " + str(move_toward(velocity.z, cur_speed, DEACCEL * delta)))
 	
 	move_and_slide()

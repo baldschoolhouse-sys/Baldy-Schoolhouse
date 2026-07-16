0b5a61350d380eccede297f5e4d1ae416f5ebce1
Jack206Man <95452460+Jack206Man@users.noreply.github.com>
2026-07-16 18:28:08 -0400

    Added Ceilings (No Height)

--- collision_shapes/tileTest/tileToGrid.gd
+++ collision_shapes/tileTest/tileToGrid.gd
@@ -26,6 +26,7 @@ extends GridMap
 @export var tileMapHeight: TileMapLayer
 @export var tileMapWalls: TileMapLayer
 @export var tileMapFloors: TileMapLayer
+@export var tileMapCeiling: TileMapLayer
 
 # Creates gridmap creation button, sets it to "create_gridmap" function
 @export_tool_button("Create Gridmap From Tilemaps") var gridMapFunction = create_gridmap
@@ -46,11 +47,12 @@ var southWall
 var eastWall
 var westWall
 var levelFloor
-var celling
+var levelCeiling
 
 # Gridmap Data
 var tileMapWallData: PackedByteArray
 var tileMapFloorData: PackedByteArray
+var tileMapCeilingData: PackedByteArray
 
 # Meshlibs
 var northMeshLib: MeshLibrary
@@ -58,6 +60,7 @@ var southMeshLib: MeshLibrary
 var eastMeshLib: MeshLibrary
 var westMeshLib: MeshLibrary
 var floorMeshLib: MeshLibrary
+var ceilingMeshLib: MeshLibrary
 
 func create_gridmap():
 	
@@ -67,6 +70,7 @@ func create_gridmap():
 	westWall = get_node("WestWall")
 	
 	levelFloor = get_node("Floor")
+	levelCeiling = get_node("Ceiling")
 	
 	# Get size of tilemap, add one to both axis because it's for some
 	# reason number one short
@@ -82,6 +86,7 @@ func create_gridmap():
 	westMeshLib = westWall.get_mesh_library()
 	
 	floorMeshLib = levelFloor.get_mesh_library()
+	ceilingMeshLib = levelCeiling.get_mesh_library()
 	
 	# Clears self to avoid corruption and errors
 	northWall.clear()
@@ -94,8 +99,36 @@ func create_gridmap():
 	tileMapWallData = tileMapWalls.get_tile_map_data_as_array()
 	# Get the floor tilemap data as a PackedByteArray
 	tileMapFloorData = tileMapFloors.get_tile_map_data_as_array()
+	tileMapCeilingData = tileMapCeiling.get_tile_map_data_as_array()
 
 	mapSize = tileMapWallData.size()
+
+	# Checks each cell/entry in tileFloorData
+	for I in range(tileMapFloorData.size()):
+		# Gets the info of the current cell
+		cellInfo = tileMapFloors.get_cell_tile_data( Vector2( I%size[0], 
+			int(I/size[0]) ) )
+		# Checks if cell is blank or not, if so, ignore 
+		if cellInfo != null:
+			# Gets provided model name from tile, then converts to string
+			modelName = String(cellInfo.get_custom_data("Model"))
+			# Checks of tile has a model name, if so, add that model to
+			# current cell
+			if modelName != "":
+				if modelName.begins_with("FloorDebug"):
+					SetCellFloor("FloorDebug", I)
+				if modelName.begins_with("BasicFloor"):
+					SetCellFloor("BasicFloor", I)
+				if modelName.begins_with("ClassicFloor"):
+					SetCellFloor("ClassicFloor", I)
+				if modelName.begins_with("CarpetFloor"):
+					SetCellFloor("CarpetFloor", I)
+				if modelName.begins_with("PlaygroundFloor"):
+					SetCellFloor("PlaygroundFloor", I)
+				if modelName.begins_with("Sidewalk"):
+					SetCellFloor("Sidewalk", I)
+				if modelName.begins_with("Grass"):
+					SetCellFloor("Grass", I)
 
 	# Checks each cell/entry in tileMapData
 	for I in range(tileMapWallData.size()):
@@ -145,10 +178,10 @@ func create_gridmap():
 						checkWall("OutsideWall", modelDirection, I, H)
 				H += 1
 	
-	# Checks each cell/entry in tileMapData
-	for I in range(tileMapFloorData.size()):
+	# Checks each cell/entry in tileCeilingData
+	for I in range(tileMapCeilingData.size()):
 		# Gets the info of the current cell
-		cellInfo = tileMapFloors.get_cell_tile_data( Vector2( I%size[0], 
+		cellInfo = tileMapCeiling.get_cell_tile_data( Vector2( I%size[0], 
 			int(I/size[0]) ) )
 		# Checks if cell is blank or not, if so, ignore 
 		if cellInfo != null:
@@ -157,20 +190,18 @@ func create_gridmap():
 			# Checks of tile has a model name, if so, add that model to
 			# current cell
 			if modelName != "":
-				if modelName.begins_with("FloorDebug"):
-					SetCellFloor("FloorDebug", I)
-				if modelName.begins_with("BasicFloor"):
-					SetCellFloor("BasicFloor", I)
-				if modelName.begins_with("ClassicFloor"):
-					SetCellFloor("ClassicFloor", I)
-				if modelName.begins_with("CarpetFloor"):
-					SetCellFloor("CarpetFloor", I)
-				if modelName.begins_with("PlaygroundFloor"):
-					SetCellFloor("PlaygroundFloor", I)
-				if modelName.begins_with("Sidewalk"):
-					SetCellFloor("Sidewalk", I)
-				if modelName.begins_with("Grass"):
-					SetCellFloor("Grass", I)
+				if modelName.begins_with("CeilingDebug"):
+					SetCellCeiling("CeilingDebug", I, 0)
+				if modelName.begins_with("BasicCeiling"):
+					SetCellCeiling("BasicCeiling", I, 0)
+				if modelName.begins_with("VentCeiling"):
+					SetCellCeiling("VentCeiling", I, 0)
+				if modelName.begins_with("FalseLight"):
+					SetCellCeiling("FalseLight", I, 0)
+
+func SetCellCeiling(type, index, tileHeight):
+	levelCeiling.set_cell_item(Vector3i(index%size[0], tileHeight, int(index/size[0])), 
+	ceilingMeshLib.find_item_by_name(type), 0)
 
 func SetCellFloor(type, index):
 	levelFloor.set_cell_item(Vector3i(index%size[0], 0, int(index/size[0])), 
